9aa056686c9b30de81995a85731032ca5abee236
bayramkarahan <44923603+bayramkarahan@users.noreply.github.com>
2020-12-11 16:04:15 +0300

    Add files via upload

--- singleinstance.cpp
+++ singleinstance.cpp
@@ -1,10 +1,10 @@
 /*****************************************************************************
- *   Copyright (C) 2016 by Yunusemre Senturk                                 *
- *   <yunusemre.senturk@pardus.org.tr>                                       *
+ *   Copyright (C) 2020 by Bayram KARAHAN                                    *
+ *   <bayramk@gmail.com>                                                     *
  *                                                                           *
  *   This program is free software; you can redistribute it and/or modify    *
  *   it under the terms of the GNU General Public License as published by    *
- *   the Free Software Foundation; either version 2 of the License, or       *
+ *   the Free Software Foundation; either version 3 of the License, or       *
  *   (at your option) any later version.                                     *
  *                                                                           *
  *   This program is distributed in the hope that it will be useful,         *
